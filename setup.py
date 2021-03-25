a23ed5d2479b29fe540073f19cbd3b02dfaaf5f7
Geoff Oakham <goakham@freshbooks.com>
2021-03-25 13:54:31 +0000

    Bump version from 1.16.0 to 1.17.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(p.join(p.dirname(__file__), 'README.md'), 'r') as f:
 
 setup(
     name='sparkplug',
-    version='1.16.0',
+    version='1.17.0',
     maintainer='FreshBooks',
     maintainer_email='dev@freshbooks.com',
     url='https://github.com/freshbooks/sparkplug/',
